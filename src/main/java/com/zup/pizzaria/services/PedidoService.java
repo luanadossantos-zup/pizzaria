76f60c712fde20a6030a522e0916b955696ce7bc
Luana Silva <luana.dossantos@zup.com.br>
2025-01-16 14:11:27 -0300

    refactor: adiciona valorTotal à classe Pedido

--- src/main/java/com/zup/pizzaria/services/PedidoService.java
+++ src/main/java/com/zup/pizzaria/services/PedidoService.java
@@ -30,6 +30,6 @@ public class PedidoService {
                 .findById(pedido.getClienteId())
                 .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
 
-        return new PedidoDTO(cliente.getNome(), cliente.getEmail(), pedido.getDescricao());
+        return new PedidoDTO(cliente.getNome(), cliente.getEmail(), pedido.getDescricao(), pedido.getValorTotal());
     }
 }
