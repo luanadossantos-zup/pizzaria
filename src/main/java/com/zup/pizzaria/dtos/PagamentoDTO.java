cbff7dba1c5382b3c81deea23edc1b2eebe33def
Luana Silva <luana.dossantos@zup.com.br>
2025-01-16 10:21:20 -0300

    feat: adiciona classe PagamentoDTO

--- src/main/java/com/zup/pizzaria/dtos/PagamentoDTO.java
+++ src/main/java/com/zup/pizzaria/dtos/PagamentoDTO.java
@@ -1,4 +1,60 @@
 package com.zup.pizzaria.dtos;
 
+import java.util.Date;
+
 public class PagamentoDTO {
+
+    private String idCliente;
+    private Long pedidoId;
+    private Enum formaPagamento;
+    private double valorPago;
+    private Date dataHoraPagamento;
+
+    public PagamentoDTO(String idCliente, Long pedidoId, Enum formaPagamento, double valorPago, Date dataHoraPagamento) {
+        this.idCliente = idCliente;
+        this.pedidoId = pedidoId;
+        this.formaPagamento = formaPagamento;
+        this.valorPago = valorPago;
+        this.dataHoraPagamento = dataHoraPagamento;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Long getPedidoId() {
+        return pedidoId;
+    }
+
+    public void setPedidoId(Long pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+
+    public Enum getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(Enum formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public double getValorPago() {
+        return valorPago;
+    }
+
+    public void setValorPago(double valorPago) {
+        this.valorPago = valorPago;
+    }
+
+    public Date getDataHoraPagamento() {
+        return dataHoraPagamento;
+    }
+
+    public void setDataHoraPagamento(Date dataHoraPagamento) {
+        this.dataHoraPagamento = dataHoraPagamento;
+    }
 }
