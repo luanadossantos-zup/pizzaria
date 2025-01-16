6de12a987beab43616c834b05449de7cf10c7c4a
Luana Silva <luana.dossantos@zup.com.br>
2025-01-16 11:21:58 -0300

    feat: adiciona classe PagamentoService

--- src/main/java/com/zup/pizzaria/services/PagamentoService.java
+++ src/main/java/com/zup/pizzaria/services/PagamentoService.java
@@ -1,4 +1,33 @@
 package com.zup.pizzaria.services;
 
+import com.zup.pizzaria.dtos.PagamentoDTO;
+import com.zup.pizzaria.models.Cliente;
+import com.zup.pizzaria.models.Pagamento;
+import com.zup.pizzaria.models.Pedido;
+import com.zup.pizzaria.repository.ClienteRepository;
+import com.zup.pizzaria.repository.PagamentoRepository;
+import com.zup.pizzaria.repository.PedidoRepository;
+
 public class PagamentoService {
+
+    private final PagamentoRepository pagamentoRepository;
+    private final PedidoRepository pedidoRepository;
+
+    public PagamentoService(PagamentoRepository pagamentoRepository, PedidoRepository pedidoRepository, ClienteRepository clienteRepository) {
+        this.pagamentoRepository = pagamentoRepository;
+        this.pedidoRepository = pedidoRepository;
+
+    }
+
+    public PagamentoDTO realizarPagamento(Pagamento pagamento) {
+        pagamentoRepository.save(pagamento);
+
+        //Buscar pedido
+        Pedido pedido = pedidoRepository
+                .findById(pagamento.getPedidoId())
+                .orElseThrow(() -> new RuntimeException("Pedido não encontrado"));
+
+
+        return new PagamentoDTO(pedido.getId(), pagamento.getFormaPagamento(), pagamento.getValorPago(), pagamento.getDataHoraPagamento());
+    }
 }
