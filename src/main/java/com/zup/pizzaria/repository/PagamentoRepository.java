5fbcfdbc4c585c49a6994d699219e3bfd3e65352
Luana Silva <luana.dossantos@zup.com.br>
2025-01-16 11:23:32 -0300

    fix: deleta import de biblioteca não usada

--- src/main/java/com/zup/pizzaria/repository/PagamentoRepository.java
+++ src/main/java/com/zup/pizzaria/repository/PagamentoRepository.java
@@ -1,7 +1,6 @@
 package com.zup.pizzaria.repository;
 
 import com.zup.pizzaria.models.Pagamento;
-import com.zup.pizzaria.models.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
