775e05959b71c6f888fd2e491a3552535e76dc99
Luana Silva <luana.dossantos@zup.com.br>
2025-01-17 10:56:04 -0300

    fix: adiciona @Entity na classe Pagamento

--- src/main/java/com/zup/pizzaria/models/Pagamento.java
+++ src/main/java/com/zup/pizzaria/models/Pagamento.java
@@ -1,11 +1,13 @@
 package com.zup.pizzaria.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.util.Date;
 
+@Entity
 public class Pagamento {
 
     @Id
